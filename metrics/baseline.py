5a95dc021111862a2e5fbf2b6f0f97f278854058
npolyakova <natusj13@mail.ru>
2025-10-01 10:03:44 -0700

    Add one more cluster type

--- metrics/baseline.py
+++ metrics/baseline.py
@@ -69,22 +69,25 @@ def compare_results(true_values, values):
 text_file = open("test-data.txt", "r")
 lines = text_file.read().split(';')
 
-true_clusters = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-                 7, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,  10, 10, 10,
-                 10, 10, 10, 10, 10, 8, 11, 11, 11, 12, 12, 13, 13, 13, 14]
+true_clusters_by_type = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                         7, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10,
+                         10, 10, 10, 10, 10, 8, 11, 11, 11, 12, 12, 13, 13, 13, 14]
+true_clusters_by_assignee = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 3,
+                             1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 3, 4, 2, 2, 2, 2,
+                             1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 2, 1, 1, 2, 2, 3, 2, 1,
+                             1, 1, 3, 3, 3, 1]
 
 # Проверка рандомной кластеризации
-print("Проверка рандомной кластеризации")
-print(compare_results(true_clusters, random_baseline(lines)), "%")
+print("Успех рандомной кластеризации по типам ", compare_results(true_clusters_by_type, random_baseline(lines)), "%")
+print("Успех рандомной кластеризации по ответственным ", compare_results(true_clusters_by_assignee, random_baseline(lines, 4)), "%")
 
 # Проверка полного совпадения текста
-print("Проверка полного совпадения текста")
-print(compare_results(true_clusters, exact_match_baseline(lines)), "%")
+print("Успех полного совпадения текста", compare_results(true_clusters_by_type, exact_match_baseline(lines)), "%")
 
 # Проверка совпадения по ключевым словам
-print("Проверка совпадения по ключевым словам")
-print(compare_results(true_clusters, tfidf_kmeans_baseline(lines, true_clusters, 12)), "%")
+print("Проверка совпадения по ключевым словам при делении по типам", compare_results(true_clusters_by_type, tfidf_kmeans_baseline(lines, true_clusters_by_type, 12)), "%")
+print("Проверка совпадения по ключевым при делении по ответственным", compare_results(true_clusters_by_assignee, tfidf_kmeans_baseline(lines, true_clusters_by_assignee, 12)), "%")
 
 # Проверка совпадения по первым словам
-print("Проверка совпадения по первым словам")
-print(compare_results(true_clusters, first_words_baseline(lines, 1)), "%")
+print("Проверка совпадения по первым словам при делении по типам ", compare_results(true_clusters_by_type, first_words_baseline(lines, 1)), "%")
+print("Проверка совпадения по первым словам при делении по ответственным ", compare_results(true_clusters_by_assignee, first_words_baseline(lines, 1)), "%")
